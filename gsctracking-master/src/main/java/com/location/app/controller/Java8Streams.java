428446cf74df11d9d157af962b9cb45993e8733b
Balamurali Pati <balamurali.pati@indiaideas.com>
2019-07-12 18:05:42 +0530

    commit ctrl shift f

--- gsctracking-master/src/main/java/com/location/app/controller/Java8Streams.java
+++ gsctracking-master/src/main/java/com/location/app/controller/Java8Streams.java
@@ -8,40 +8,39 @@ public class Java8Streams {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		List<Product> productsList = new ArrayList<Product>();  
-        //Adding Products  
-        productsList.add(new Product(1,"HP Laptop",25000f));  
-        productsList.add(new Product(2,"Dell Laptop",30000f));  
-        productsList.add(new Product(3,"Lenevo Laptop",28000f));  
-        productsList.add(new Product(4,"Sony Laptop",28000f));  
-        productsList.add(new Product(5,"Apple Laptop",90000f));  
-        
-        productsList.stream().forEach(product -> System.out.println(product.name));  
-       
-        List<Float> productPriceList2 =productsList.stream()  
-                                     .filter(p -> p.price > 30000)// filtering data  
-                                     .map(p->p.price)        // fetching price  
-                                     .collect(Collectors.toList()); // collecting as list  
-        System.out.println(productPriceList2);  
-        
-        Product productA = productsList.stream()  
-        .max((product1, product2)->   
-        product1.price > product2.price ? -1: 1).get();  
 
-System.out.println("Produyct a price"+productA.price);  
+		List<Product> productsList = new ArrayList<Product>();
+		// Adding Products
+		productsList.add(new Product(1, "HP Laptop", 25000f));
+		productsList.add(new Product(2, "Dell Laptop", 30000f));
+		productsList.add(new Product(3, "Lenevo Laptop", 28000f));
+		productsList.add(new Product(4, "Sony Laptop", 28000f));
+		productsList.add(new Product(5, "Apple Laptop", 90000f));
+
+		productsList.stream().forEach(product -> System.out.println(product.name));
+
+		List<Float> productPriceList2 = productsList.stream().filter(p -> p.price > 30000)// filtering data
+				.map(p -> p.price) // fetching price
+				.collect(Collectors.toList()); // collecting as list
+		System.out.println(productPriceList2);
+
+		Product productA = productsList.stream().max((product1, product2) -> product1.price > product2.price ? -1 : 1)
+				.get();
+
+		System.out.println("Produyct a price" + productA.price);
 
 	}
 
 }
 
-class Product{  
-    int id;  
-    String name;  
-    float price;  
-    public Product(int id, String name, float price) {  
-        this.id = id;  
-        this.name = name;  
-        this.price = price;  
-    }  
-} 
+class Product {
+	int id;
+	String name;
+	float price;
+
+	public Product(int id, String name, float price) {
+		this.id = id;
+		this.name = name;
+		this.price = price;
+	}
+}
